eace7e4e31a3d3e83e0d37ff0175efa7b53bfcf0
Soundarajan N <rajsoundar2001@gmail.com>
2026-01-31 17:36:15 +0800

    Update hello.py
    code updated(1,2,3)

--- hello.py
+++ hello.py
@@ -3,3 +3,26 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# Find all the factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10 % 3 = 1
+x = 52633
+for i in range(2, x):
+    if x%i ==0:
+        print(i)
+
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
+            
+# Write a program to find all factors of the numbers in the list l
+l = [52633, 8137, 1024, 999]
+
+for x in l:
+    print("Factors of", x, ":")
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
+    print() #the blank line between  numbers
